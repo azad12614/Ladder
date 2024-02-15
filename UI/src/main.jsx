6ce09f93b4c338db3a32ef1dc99dfa01439f8307
azad12614 <azad.jishan2003@gmail.com>
2024-02-15 14:29:22 +0600

    4th

--- UI/src/main.jsx
+++ UI/src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-const URL = `http://localhost:3000`;
+//const URL = `http://localhost:3000`;
+const URL = `https://ladder-backend.onrender.com`;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
