3130c297845537b910784faa7f5dae4114b8ccfb
azad12614 <azad.jishan2003@gmail.com>
2025-03-21 18:52:11 +0600

    code clean

--- UI/src/App.jsx
+++ UI/src/App.jsx
@@ -1,36 +1,3 @@
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import React, { useState } from "react";
-// import "./App.css";
-// import Home from "./pages/Home/Home";
-// import AddProblem from "./pages/AddProblem/AddProblem";
-// import ViewProblem from "./pages/ViewProblem/ViewProblem";
-
-// function App(URL) {
-//   const [Handle, setHandle] = useState("Enter CF Handle");
-
-//   function sendValue(Handle) {
-//     setHandle(`${Handle}`);
-//   }
-
-//   const router = createBrowserRouter([
-//     {
-//       path: "/",
-//       element: <Home />,
-//     },
-//     {
-//       path: "/add-problem",
-//       element: <AddProblem URL={URL} />,
-//     },
-//     {
-//       path: "/view-problem",
-//       element: <ViewProblem Handle={Handle} sendValue={sendValue} />,
-//     },
-//   ]);
-//   return <RouterProvider router={router} />;
-// }
-
-// export default App;
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import React, { useState } from "react";
 import "./App.css";
